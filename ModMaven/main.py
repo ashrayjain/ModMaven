128a805da98d2b52e32378152e20f45fc99065bc
Karan Kamath <artemisfowl2.karan@gmail.com>
2013-06-09 20:14:36 +0530

    Integrated module page, module search, and module presence verification

--- ModMaven/main.py
+++ ModMaven/main.py
@@ -1,6 +1,8 @@
 import webapp2
 import jinja2
 import os
+import json
+import cgi
 
 jinja_environment = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__) + "/templates"))
@@ -10,6 +12,29 @@ class MainPage(webapp2.RequestHandler):
     def get(self):
         template = jinja_environment.get_template('main.html')
         self.response.out.write(template.render())
+        
+class ModPage(webapp2.RequestHandler):
+    """ Handler for the module page."""
+    def post(self):
+        input = self.request.get('modName')
+        json_data = open("modInfo.json")
+        data = json.load(json_data, encoding='latin1')
+        if input in data:
+            template = jinja_environment.get_template('modulepage.html')
+        
+            template_values = {
+                'modName': input,
+            }
+            self.response.out.write(template.render(template_values))
+        else:
+            template = jinja_environment.get_template('main.html')
+            template_values = {
+                'NotFoundError': "The Module You Requested Was Not Found",
+            }
+            self.response.out.write(template.render(template_values))
+        json_data.close()    
+       
 
-app = webapp2.WSGIApplication([('/', MainPage)],
+app = webapp2.WSGIApplication([('/', MainPage),
+                              ('/modpage', ModPage)],
                               debug=True)
