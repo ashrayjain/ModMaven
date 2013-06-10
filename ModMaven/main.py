ba9e497e2aaf199c3358c42b51718735a06b042a
Ashray Jain <ashrayj11@gmail.com>
2013-06-11 00:54:00 +0300

    Fixed search backend and other minor css tweaks

--- ModMaven/main.py
+++ ModMaven/main.py
@@ -2,41 +2,51 @@ import webapp2
 import jinja2
 import os
 import json
-import cgi
 
-jinja_environment = jinja2.Environment(
-    loader=jinja2.FileSystemLoader(os.path.dirname(__file__) + "/templates"))
 
-class MainPage(webapp2.RequestHandler):
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+
+jinja_environment = jinja2.Environment(loader=jinja2.FileSystemLoader(template_dir),
+                                       autoescape = True)
+
+class Handler(webapp2.RequestHandler):
+    """Handler Class with Utility functions for Templates"""
+    def __write__(self, *a, **kw):
+        self.response.out.write(*a, **kw)
+
+    def __render_str__(self, template, **params):
+        t = jinja_environment.get_template(template)
+        return t.render(params)
+
+    def render(self, template, **kw):
+        self.__write__(self.__render_str__(template, **kw))
+
+
+class MainPage(Handler):
     """ Handler for the front page."""
     def get(self):
-        template = jinja_environment.get_template('main.html')
-        self.response.out.write(template.render())
-        
-class ModPage(webapp2.RequestHandler):
+        isError = self.request.cookies.get('error')
+        error = ""
+        if isError:
+            error = "The Module You Requested Was Not Found"
+            self.response.headers.add_header('Set-Cookie', 'error=false; Path=/; Expires=Thu, 01-Jan-1970 00:00:00 GMT')
+        self.render('main.html', NotFoundError=error)
+
+
+class ModPage(Handler):
     """ Handler for the module page."""
-    def post(self):
-        input = self.request.get('modName')
+    def get(self):
+        modName = self.request.get('modName').upper()
         json_data = open("modInfo.json")
         data = json.load(json_data, encoding='latin1')
-        if input in data:
-            template = jinja_environment.get_template('modulepage.html')
-        
-            template_values = {
-                'modName': input,
-                'modTitle': data[input]["title"],
-                'modDesc': data[input]["description"]
-            }
-            self.response.out.write(template.render(template_values))
+        if modName in data:
+            self.render("modulepage.html", modName=modName, modTitle=data[modName]["title"], modDesc=data[modName]["description"])
+            json_data.close()
         else:
-            template = jinja_environment.get_template('main.html')
-            template_values = {
-                'NotFoundError': "The Module You Requested Was Not Found",
-            }
-            self.response.out.write(template.render(template_values))
-        json_data.close()    
-       
+            json_data.close()
+            self.response.headers.add_header('Set-Cookie', 'error=true; Path=/')
+            self.redirect('/')
 
-app = webapp2.WSGIApplication([('/', MainPage),
-                              ('/modpage', ModPage)],
+app = webapp2.WSGIApplication([('/modpage/?', ModPage),
+                               ('/.*', MainPage)],
                               debug=True)
