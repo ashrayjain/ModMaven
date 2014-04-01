29c6e4c18a1c9923923ea4a6e95edb4e0a16426c
Ashray Jain <ashrayj11@gmail.com>
2014-04-02 05:20:32 +0800

    Added automatic cron jobs for user ivle data update every sem!

--- ModMaven/js/ivle.js
+++ ModMaven/js/ivle.js
@@ -1,9 +1,9 @@
 function closePopup(popup) {
     if (popup.location.href !== undefined && popup.location.href.indexOf("http://nusmodmaven.appspot.com/ivle") === 0){
         clearInterval(popupClose);
-        //console.log("Sent");
-        $.post('/ivle', {token: (popup.location.href.split("token=")[1]).split("&")[0]});
-        popup.close();
-        $("#ivle-alert").alert('close');
+        $.post('/ivle', {token: (popup.location.href.split("token=")[1]).split("&")[0]}, function(data) {
+            popup.close();
+            $("#ivle-alert").alert('close');
+        });
     }
 }
